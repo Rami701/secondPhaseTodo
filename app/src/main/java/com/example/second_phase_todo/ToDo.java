b8b9d4691d6d2376dc67db6573da887196ed2cc1
r4mim <r4mimu701@gmail.com>
2023-02-06 20:18:45 +0200

    phase2Todo

--- app/src/main/java/com/example/second_phase_todo/ToDo.java
+++ app/src/main/java/com/example/second_phase_todo/ToDo.java
@@ -3,18 +3,7 @@ package com.example.second_phase_todo;
 public class ToDo {
     private String title, details, date;
 
-    public static final ToDo[] toDos = {
-            new ToDo("1", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("2", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("3", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("4", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("5", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("6", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("7", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("8", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("9", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-            new ToDo("10", "bla bla bla ;laskdfj;alskfj;alskf;las g;las gla;ksjg erjlahgqlkjfhLKWHFL KEWJHFKLAEJRHG KLJERGH K;JLEHG KL;J", "12/3/2002"),
-    };
+
 
     public ToDo(String title, String details, String date) {
         this.title = title;
