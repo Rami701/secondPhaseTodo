98048630ee827aa504cfa8c4cf81f87b60667bd5
r4mim <r4mimu701@gmail.com>
2023-02-08 18:44:36 +0200

    Update - deleting the fake data

--- app/src/main/java/com/example/second_phase_todo/Grade.java
+++ app/src/main/java/com/example/second_phase_todo/Grade.java
@@ -3,15 +3,7 @@ package com.example.second_phase_todo;
 public class Grade {
     private String title, grade, realGrade;
 
-    public static final Grade[] grades= {
-            new Grade("Quiz 1", "90 / 100", "9 / 10"),
-            new Grade("Quiz 2", "80 / 100", "8 / 10"),
-            new Grade("Midterm", "66 / 100", "12 / 20"),
-            new Grade("Quiz 3", "90 / 100", "9 / 10"),
-            new Grade("Quiz 4", "90 / 100", "9 / 10"),
-            new Grade("Quiz 5", "0 / 100", "0 / 10"),
-            new Grade("Quiz 6", "100 / 100", "10 / 10"),
-    };
+
 
     public Grade(String title, String grade, String realGrade) {
         this.title = title;
